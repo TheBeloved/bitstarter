2ba8e3c46fbc63c2807ac1dd3f05778dddf0fcec
Femi Banjo <femibanjo@hotmail.com>
2013-08-27 21:18:27 +0000

    added you are gorge

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html", "utf-8"));
+  response.send("you are so gorgeous");
 });
 
 var port = process.env.PORT || 5000;
