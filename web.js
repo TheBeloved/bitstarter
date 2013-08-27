ad2f1ad773cf7e86b790c1cbccd59905a13dc764
Femi Banjo <femibanjo@hotmail.com>
2013-08-27 20:57:30 +0000

    changed greeting

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hi Beautiful thing!');
 });
 
 var port = process.env.PORT || 5000;
